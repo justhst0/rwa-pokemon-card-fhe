50613ec35b8a59977e57a33b1b8c8b92e8d333c6
justhst0 <hstptcn0@gmail.com>
2025-10-31 16:00:34 +0700

    config gitignore
    config gitignore

--- home/src/config/contracts.ts
+++ home/src/config/contracts.ts
@@ -189,23 +189,6 @@ export const CONTRACT_ABI = [
   },
   {
     "inputs": [
-      { "internalType": "address", "name": "operator", "type": "address" },
-      { "internalType": "bool", "name": "approved", "type": "bool" }
-    ],
-    "name": "setApprovalForAll",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "symbol",
-    "outputs": [ { "internalType": "string", "name": "", "type": "string" } ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
       { "internalType": "address", "name": "from", "type": "address" },
       { "internalType": "address", "name": "to", "type": "address" },
       { "internalType": "uint256", "name": "tokenId", "type": "uint256" }
